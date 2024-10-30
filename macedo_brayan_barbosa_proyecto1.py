38c09256c7a14c6f181d31e0788489d696cb72b2
Brayan Macedo <macedobarbosab@gmail.com>
2024-10-30 09:07:46 +0100

    añadida función que muestra la opción del jugador

--- macedo_brayan_barbosa_proyecto1.py
+++ macedo_brayan_barbosa_proyecto1.py
@@ -1,6 +1,7 @@
 puntos_jugador = 0
 puntos_ordenador = 0
 
+# Número aleatorio que elige el ordenador, las funciones cerradas están terminadas.
 def ordenador():
     import random
     return random.randint(0, 2)
@@ -9,9 +10,9 @@ def ordenador():
 def menu():
     print("Bienvenido al piedra, papel, tijera, lagarto o spock")
     print('''Escoge tu opción:
-          1. Piedra
-          2. Papel
-          3. Tijera''')
+          0. Piedra
+          1. Papel
+          2. Tijera''')
         # 4. Lagarto 5. Spock
     
 def comparar_jugadas(opcion_ordenador, jugada_jugador):
@@ -29,11 +30,21 @@ def opcion_ordenador(num_ordenador):
         case 2:
             print("Ordenador eligió Tijera")
 
+def opcion_jugador(num_jugador):
+    match num_jugador:
+        case 0:
+            print("Jugador eligió Piedra")
+        case 1:
+            print("Jugador eligió Papel")
+        case 2:
+            print("Jugador eligió Tijera")
+
 menu()
-jugada_jugador = int(input("Elige tu opción: "))
+num_jugador = int(input("Elige tu opción: "))
 
 
 opcion_ordenador(num_ordenador)
+opcion_jugador(num_jugador)
 
 
 
