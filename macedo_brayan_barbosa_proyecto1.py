5812c1c438fd4ef75b7072b3fec5d90023cee6ec
Brayan Macedo <macedobarbosab@gmail.com>
2024-10-30 10:20:37 +0100

    las comparaciones funcionan y el bucle muestra el contador de puntos.

--- macedo_brayan_barbosa_proyecto1.py
+++ macedo_brayan_barbosa_proyecto1.py
@@ -13,30 +13,45 @@ def menu():
           2. Tijera''')
         # 4. Lagarto 5. Spock
 
-puntos_jugador = 0
-puntos_ordenador = 0
-
-# falta añadir el empate, lagarto y spock
-def comparar_jugadas(num_ordenador, num_jugador, puntos_jugador, puntos_ordenador):
+# falta añadir lagarto y spock.
+def comparar_jugadas(num_ordenador, num_jugador):
+    victoria_ordenador = False
     if num_ordenador == 0 and num_jugador == 2:
         print("Victoria para ordenador")
-        puntos_ordenador += 1 
+        victoria_ordenador = True
+        return victoria_ordenador
     elif num_ordenador == 1 and num_jugador == 0:
         print("Victoria para ordenador")
-        puntos_ordenador += 1
+        victoria_ordenador = True
+        return victoria_ordenador
     elif num_ordenador == 2 and num_jugador == 1:
         print("Victoria para ordenador")
-        puntos_ordenador += 1
+        victoria_ordenador = True
+        return victoria_ordenador
     elif num_jugador == 0 and num_ordenador == 2:
         print("Victoria para jugador")
-        puntos_ordenador += 1 
+        victoria_ordenador = False
+        return victoria_ordenador
     elif num_jugador == 1 and num_ordenador == 0:
         print("Victoria para jugador")
-        puntos_ordenador += 1
+        victoria_ordenador = False
+        return victoria_ordenador
     elif num_jugador == 2 and num_ordenador == 1:
         print("Victoria para jugador")
-        puntos_ordenador += 1
-
+        victoria_ordenador = False
+        return victoria_ordenador
+    elif num_jugador == 0 and num_ordenador == 0:
+        print("Ha sido empate, no se otorgan puntos")
+        victoria_ordenador = False
+        return "empate"
+    elif num_jugador == 1 and num_ordenador == 1:
+        print("Ha sido empate, no se otorgan puntos")
+        victoria_ordenador = False
+        return "empate"
+    elif num_jugador == 2 and num_ordenador == 2:
+        print("Ha sido empate, no se otorgan puntos")
+        victoria_ordenador = False
+        return "empate"
 
 # falta añadir lagarto y spock a las funciones
 def opcion_ordenador(num_ordenador):
@@ -59,15 +74,27 @@ def opcion_jugador(num_jugador):
 
 menu()
 
+puntos_jugador = 0
+puntos_ordenador = 0
 
-while puntos_jugador < 3 or puntos_ordenador < 3:
+while puntos_jugador < 3 and puntos_ordenador < 3:
         num_jugador = int(input("Elige tu opción: "))
         num_ordenador = ordenador()
 
         opcion_ordenador(num_ordenador)
         opcion_jugador(num_jugador)    
-        comparar_jugadas(num_ordenador, num_jugador, puntos_jugador, puntos_ordenador)
-        print("Recuento de puntos, Jugador:", puntos_jugador, "|", "Ordenador:", puntos_ordenador)
+        partida = comparar_jugadas(num_ordenador, num_jugador)
+        if partida == True:
+            puntos_ordenador += 1
+        elif partida == False:
+            puntos_jugador += 1
+        
+        print("Recuento de puntos:\n", "Jugador:", puntos_jugador, "|", "Ordenador:", puntos_ordenador)
+
+if puntos_jugador == 3:
+    print("Has ganado la partida, ¡Felicidades!")
+elif puntos_ordenador == 3:
+    print("El ordenador ha ganado la partida, ¡Suerte en la próxima!")
 
 # piedra > lagarto
 # piedra > tijeras
