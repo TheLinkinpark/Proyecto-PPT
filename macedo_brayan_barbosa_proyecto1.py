05e6eee266ac6b06b6b08c0375c5fa8bcce46517
Brayan Macedo <macedobarbosab@gmail.com>
2024-10-30 09:02:10 +0100

    añadida función que muestra la opción escogida por el ordenador

--- macedo_brayan_barbosa_proyecto1.py
+++ macedo_brayan_barbosa_proyecto1.py
@@ -3,7 +3,7 @@ puntos_ordenador = 0
 
 def ordenador():
     import random
-    print(random.randint(0, 4))
+    return random.randint(0, 2)
 
 
 def menu():
@@ -11,18 +11,30 @@ def menu():
     print('''Escoge tu opción:
           1. Piedra
           2. Papel
-          3. Tijera
-          4. Lagarto
-          5. Spock''')
+          3. Tijera''')
+        # 4. Lagarto 5. Spock
     
 def comparar_jugadas(opcion_ordenador, jugada_jugador):
     if opcion_ordenador == 0 and jugada_jugador == 3:
         print("Victoria para ordenador") 
 
-    
-menu()
+num_ordenador = ordenador()
 
+def opcion_ordenador(num_ordenador):
+    match num_ordenador:
+        case 0:
+            print("Ordenador eligió Piedra")
+        case 1:
+            print("Ordenador eligió Papel")
+        case 2:
+            print("Ordenador eligió Tijera")
+
+menu()
 jugada_jugador = int(input("Elige tu opción: "))
+
+
+opcion_ordenador(num_ordenador)
+
 
 
 
