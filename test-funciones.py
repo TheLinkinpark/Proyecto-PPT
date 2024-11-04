75dd2c72921558758dca471253ade61326faf8b6
Brayan Macedo <macedobarbosab@gmail.com>
2024-11-04 15:15:33 +0100

    test de funciones

---
+++ test-funciones.py
@@ -0,0 +1,8 @@
+def probas():
+    from macedo_brayan_barbosa_proyecto1 import comparar_jugadas
+    '''
+    >>> comparar_jugadas(0, 2)
+    True
+    >>> comparar_jugadas(2, 0)
+    False
+    '''
